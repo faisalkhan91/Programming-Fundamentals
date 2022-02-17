dcb8062e084eca3277e24f107dfed6b4d12b332d
Faisal Khan <faisalkhan91@outlook.com>
2022-02-17 15:27:51 -0500

    Added Space Complexity Example

--- Big O Notation/O(n^2).py
+++ Big O Notation/O(n^2).py
@@ -1,1 +1,21 @@
-//
+# This is the code that demonstrates O(n^2) or Quadratic Time complexity of an algorithm.
+# Log all pairs of an array
+
+# Method Definition
+
+def create_pairs(boxes):
+
+    pairs = []
+    for i in range(0, len(boxes)):
+        for j in range(0, len(boxes)):
+            pairs.append((boxes[i], boxes[j]))
+
+    print(pairs)
+
+
+# Declarations
+
+CONST_BOXES = [1, 2, 3, 4, 5]
+create_pairs(CONST_BOXES)
+
+# BigO (n * n) => BigO(n^2)
