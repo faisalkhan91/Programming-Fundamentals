627abbe55f7f7b0024d4c09109c8edd2780b7688
Faisal Khan <faisalkhan91@outlook.com>
2022-02-23 00:53:01 -0500

    Created array data structure and defined array methods like push, pop etc

--- Data Structures/Array/Implementation.py
+++ Data Structures/Array/Implementation.py
@@ -11,3 +11,44 @@ class Array:
         self.length = 0
         self.data = {}
 
+    def get(self, index):
+        return self.data[index]
+
+    def push(self, item):
+        self.data[self.length] = item
+        self.length += 1
+        return self.length
+
+    def pop(self):
+        last_item = self.data[self.length-1]
+        del self.data[self.length-1]
+        self.length -= 1
+        return last_item
+
+    def delete(self, index):
+        item = self.data[index]
+        self.shift_items(index)
+        return item
+
+    def shift_items(self, index):
+        for i in range(index, self.length-1):
+            self.data[i] = self.data[i+1]
+        del self.data[self.length-1]
+        self.length -= 1
+
+
+
+# Declaration
+arr = Array()
+arr.push('hi')
+arr.push('how')
+arr.push('you')
+arr.push('doing')
+#arr.pop()
+arr.delete(2)
+
+for i in range(0, arr.length):
+    print(arr.get(i))
+
+
+
