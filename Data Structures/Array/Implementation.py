fd8951d0e370cc89dec671eb78ce8a1e1bfbeb09
Faisal Khan <faisalkhan91@outlook.com>
2022-02-25 18:51:51 -0500

    Implemented insert into array data structure

--- Data Structures/Array/Implementation.py
+++ Data Structures/Array/Implementation.py
@@ -11,6 +11,9 @@ class Array:
         self.length = 0
         self.data = {}
 
+    def __str__(self):
+        return str(self.__dict__)
+
     def get(self, index):
         return self.data[index]
 
@@ -26,16 +29,28 @@ class Array:
         return last_item
 
     def delete(self, index):
-        item = self.data[index]
-        self.shift_items(index)
-        return item
-
-    def shift_items(self, index):
-        for i in range(index, self.length-1):
-            self.data[i] = self.data[i+1]
-        del self.data[self.length-1]
+        deleted_item = self.data[index]
+        self.shift_items(index, 'Delete')
+        del self.data[self.length - 1]
         self.length -= 1
+        return deleted_item
 
+    def shift_items(self, index, flag):
+
+        if flag == 'Insert':
+            for i in reversed(range(index, self.length)):
+                self.data[i] = self.data[i - 1]
+
+        elif flag == 'Delete':
+            for i in range(index, self.length-1):
+                self.data[i] = self.data[i+1]
+
+    def insert(self, index, item):
+        inserted_item = item
+        self.length += 1
+        self.shift_items(index, 'Insert')
+        self.data[index] = item
+        return inserted_item
 
 
 # Declaration
@@ -46,9 +61,13 @@ arr.push('you')
 arr.push('doing')
 #arr.pop()
 arr.delete(2)
+arr.insert(2, 'me')
+arr.insert(3, 'and you')
 
-for i in range(0, arr.length):
-    print(arr.get(i))
+# for i in range(0, arr.length):
+#     print(arr.get(i))
+
+print(arr)
 
 
 
