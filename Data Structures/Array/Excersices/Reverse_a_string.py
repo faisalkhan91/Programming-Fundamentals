0aff76351abfda6b4811685293a09f28d6b98962
Faisal Khan <faisalkhan91@outlook.com>
2022-02-27 00:52:02 -0500

    Created solutionn for reverse a string problem.

--- Data Structures/Array/Excersices/Reverse_a_string.py
+++ Data Structures/Array/Excersices/Reverse_a_string.py
@@ -5,17 +5,35 @@ For example string 'Hi My name is Faisal' should be 'lasiaF si eman yM iH'
 
 
 # Function Definition
+
+# First attempt to reverse a string.
 def reverse_string(string_input):
     split_string = list(string_input)
-
     reversed_string = []
-
     for i in reversed(range(len(split_string))):
         reversed_string.append(split_string[i])
-
     print(reversed_string)
+
+
+# Reverse a string using Andrei's solution to reverse a string exercise.
+def reverse_string_2(string_input):
+    # Check the input for errors, What if the string is a number or undefined?
+    if type(string_input) != str or len(string_input) < 2:
+        return 'Please enter valid input!'
+
+    backwards = []
+    total_items = len(string_input) - 1
+
+    for i in range(total_items, -1, -1):
+        backwards.append(string_input[i])
+    print(backwards)
+
+    return ''.join(backwards)
 
 
 # Declarations
 to_reverse = 'hello'
-reverse_string(to_reverse)
+# reverse_string(to_reverse)
+string = reverse_string_2(to_reverse)
+
+print(string)
