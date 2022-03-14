3603d6309004153739a6d2d6a2fbb2ad0739478e
Faisal Khan <faisalkhan91@outlook.com>
2022-03-14 18:53:16 -0400

    Started implementing Linked List Data Structure in Python.

--- Data Structures/Linked List/Implementation.py
+++ Data Structures/Linked List/Implementation.py
@@ -2,18 +2,13 @@
 Implementation of Linked List Data Structure in python.
 """
 
+
 # Function Definition
 
+class LinkedList:
 
-
-
-
-
-
-
-
-
-
-
+    def __init__(self):
+        pass
 
 # Declaration
+
