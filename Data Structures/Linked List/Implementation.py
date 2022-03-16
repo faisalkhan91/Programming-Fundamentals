30f5bf36ab373f14826b78f5a9bf137d6b11b8f5
Faisal Khan <faisalkhan91@outlook.com>
2022-03-16 13:07:42 -0400

    Created separate class node to encapsulate.

--- Data Structures/Linked List/Implementation.py
+++ Data Structures/Linked List/Implementation.py
@@ -3,20 +3,31 @@ Implementation of Linked List Data Structure in python.
 """
 
 
-# Function Definition
+# Classes
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return str(self.data)
+
 
 class LinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.length = 1
+        self.head = Node()
 
     def __str__(self):
-        return str(self.__dict__)
+        return str(self.head)
 
-    
+    def append(self, value):
+        self.head = Node(value, self.head)
+        self.length += 1
 
 
 # Declaration
 linked = LinkedList()
+linked.append(20)
 print(linked)
+
