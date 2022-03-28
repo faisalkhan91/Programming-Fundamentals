603881ba90a71628bd47f80506ef030b68aa79e2
Faisal Khan <faisalkhan91@outlook.com>
2022-03-28 16:16:55 -0400

    Completed stack implementation using linked list.

--- Data Structures/Stack/Implementation.py
+++ Data Structures/Stack/Implementation.py
@@ -26,6 +26,7 @@ class Stack:
     # Method to check and return the value of the top node from the stack.
     def peek(self):
         if self.top is not None:
+            print("The top node is: ", self.top)
             return self.top
         else:
             print("Stack is empty.")
@@ -33,12 +34,22 @@ class Stack:
     # Method to add a new node at the top of the stack.
     def push(self, value):
         new_node = Node(value)
-        new_node.next = self.top
-        self.top = new_node
+        if self.length == 0:
+            self.top = new_node
+            self.bottom = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.length += 1
+        return self
 
     # Method to remove the top node from the stack.
     def pop(self):
+        if self.top is None:
+            print("Stack is empty.")
+            return None
         self.top = self.top.next
+        self.length -= 1
 
     # Method to check if the stack is empty.
     def is_empty(self):
@@ -47,6 +58,25 @@ class Stack:
         else:
             print(self.length, " elements are in the stack.")
 
+    def display_stack(self):
+        current_node = self.top
+        print("***Stack***")
+        while current_node is not None:
+            print(current_node.data)
+            print("↓")
+            current_node = current_node.next
+        print("None")
+        print("Top of the stack: ", self.top)
+        print("Bottom of the stack: ", self.bottom)
+        print("Length of the stack: ", self.length)
+
 
 # Main
 my_stack = Stack()
+my_stack.peek()
+my_stack.push("Google")
+my_stack.push("LinkedIn")
+my_stack.push("Reddit")
+my_stack.peek()
+my_stack.pop()
+my_stack.display_stack()
