076cad8102abbb2299de8f8fe12a78ddf2cba476
Faisal Khan <faisalkhan91@outlook.com>
2022-04-01 23:45:23 -0400

    Created an outline for Binary Search Tree implementation.

---
+++ Data Structures/Tree/Binary Tree/Binary Search Tree/Implementation.py
@@ -0,0 +1,29 @@
+"""
+Implementation of Binary Search Tree data structure in python.
+"""
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def lookup(self):
+        pass
+
+
+# Initialization
+
+my_BST = BinarySearchTree()
