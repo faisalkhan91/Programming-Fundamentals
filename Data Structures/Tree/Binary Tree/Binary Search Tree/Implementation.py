671cb409ca8a54e058ef69cf26a018a72e4819cf
Faisal Khan <faisalkhan91@outlook.com>
2022-04-03 01:03:26 -0400

    BST insert initial commit.

--- Data Structures/Tree/Binary Tree/Binary Search Tree/Implementation.py
+++ Data Structures/Tree/Binary Tree/Binary Search Tree/Implementation.py
@@ -14,12 +14,24 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    def __str__(self):
+        return str(self.__dict__)
+
     def insert(self, value):
         new_node = Node(value)
         if self.root is None:
             self.root = new_node
         else:
-            pass
+            current_node = self.root
+            while current_node.left is not None or current_node.right is not None:
+                if new_node.data > current_node.data:
+                    current_node = current_node.right
+                elif new_node.data < current_node.data:
+                    current_node = current_node.left
+            if current_node.left is None:
+                current_node.left = new_node
+            else:
+                current_node.right = new_node
 
     def delete(self):
         pass
@@ -27,7 +39,15 @@ class BinarySearchTree:
     def lookup(self):
         pass
 
+    def print(self):
+        pass
+
 
 # Initialization
 
 my_BST = BinarySearchTree()
+my_BST.insert(10)
+my_BST.insert(5)
+my_BST.insert(3)
+my_BST.insert(6)
+print(my_BST.root.left.left.data)
