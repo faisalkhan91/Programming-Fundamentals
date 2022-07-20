7cf9b86e44417079465088cbe312d9589002cbd8
Faisal Khan <faisalkhan91@outlook.com>
2022-07-19 23:55:26 -0400

    Added comments.

--- Algorithms/Sorting/Insertion Sort.py
+++ Algorithms/Sorting/Insertion Sort.py
@@ -1,44 +1,40 @@
 """
-Implementation of the insertion sort algorithm in python.
+Implementation of the insertion sort algorithm in python. In insertion sort algorithm the array is divided into sorted
+and unsorted part and each element is compared ot the sorted part and inserted in the correct order.
 
-Time complexity of Insertion Sort Algorithm is O(n^2) and the algorithm is fast when the array is already sorted.
+Time complexity of Insertion Sort Algorithm is O(n^2) and the algorithm is fast when the array is already sorted as the
+time complexity is O(n) in that case. Space complexity is O(1) as no new data structures are created.
 """
 
 
 # Function Definition
 
-# Reference:
-# https://stackoverflow.com/questions/72987595/implementation-of-insertion-sort-algorithm-in-python/72989757#72989757
+# Initial attempt to create an insertion sort algorithm.
 def insertion_sort_initial(array):
-    for i in range(1, len(array)):
-        for j in reversed(range(1, i+1)):
-            if array[j-1] > array[j]:
+    for i in range(1, len(array)):  # Keep track of the sorted part of the array.
+        for j in reversed(range(1, i+1)):  # Loop to insert into the correct position in the array.
+            if array[j-1] > array[j]:  # Keep swapping until the element to the right is smaller than the current one.
                 temp = array[j-1]
                 array[j-1] = array[j]
                 array[j] = temp
     return array
 
 
+# Optimized version of the code to reduce swaps if the element is on the left is smaller than the right element, i.e. an
+# already sorted array.
+# Reference:
+# https://stackoverflow.com/questions/72987595/implementation-of-insertion-sort-algorithm-in-python/72989757#72989757
 def insertion_sort_optimized(array):
-    for i in range(1, len(array)):
-        for j in range(i, 0, -1):
-            if array[j-1] <= array[j]:
+    for i in range(1, len(array)):  # Keep track of the sorted part of the array.
+        for j in range(i, 0, -1):  # Loop to insert into the correct position in the array.
+            if array[j-1] <= array[j]:  # If the array to sort is larger than the array on the left, exit loop.
                 break
+            # Do swap
             temp = array[j-1]
             array[j-1] = array[j]
             array[j] = temp
     return array
 
-
-def insertion_sort_optimized(array):
-    for i in range(1, len(array)):
-        for j in range(i, 0, -1):
-            if array[j-1] <= array[j]:
-                break
-            temp = array[j-1]
-            array[j-1] = array[j]
-            array[j] = temp
-    return array
 
 # Declaration
 
