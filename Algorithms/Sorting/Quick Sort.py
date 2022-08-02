afc63fa1240f4c8b16fe7562ed076d796eb0e533
Faisal Khan <faisalkhan91@outlook.com>
2022-08-02 19:20:55 -0400

    Initial commit for quick sort and updates for merge sort.

---
+++ Algorithms/Sorting/Quick Sort.py
@@ -0,0 +1,20 @@
+"""
+Implementation of Quick Sort algorithm in python. The sorting technique used in this sort is to select a pivot randomly
+and move items smaller than the pivot to left and items greater than the pivot to the right, then divide and conquer
+strategy is used divide into smaller sub arrays and sorted in a similar fashion using a pivot until the sub array is
+sorted.
+
+The time complexity of Quick Sort algorithm is O(nlogn). Space complexity is O(logn).
+"""
+
+
+# Function Definition
+
+def quick_sort(array):
+    pass
+
+
+# Declaration
+
+to_sort = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+print(quick_sort(to_sort))
