6bf85f6fe4da718a54582f6a5e9b05bed37415dd
Faisal Khan <faisalkhan91@outlook.com>
2022-08-11 00:42:36 -0400

    BFS initial commit.

---
+++ Algorithms/Searching/Breadth First Search.py
@@ -0,0 +1,7 @@
+"""
+Implementation of Breadth First Search algorithm in python. Breadth First Search approach searches all the nodes level
+by level from left to right starting from the root node. We keep track of all the children of each node visited.
+
+Time complexity is O(n).
+"""
+
