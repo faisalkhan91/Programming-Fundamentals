ed2bd16e514c18862819642623abec4a3f0ead7a
Faisal Khan <faisalkhan91@outlook.com>
2022-10-14 00:26:14 -0400

    206. Reverse Linked List

--- Data Structures/Linked List/Exercises/LeetCode/206. Reverse Linked List.py
+++ Data Structures/Linked List/Exercises/LeetCode/206. Reverse Linked List.py
@@ -3,6 +3,7 @@
 https://leetcode.com/problems/reverse-linked-list/?envType=study-plan&id=level-1
 """
 
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -10,4 +11,10 @@ https://leetcode.com/problems/reverse-linked-list/?envType=study-plan&id=level-1
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        reversed = None  # Set reversed to None
+        while head:  # as long as there are nodes present.
+            temp = head  # Store the current element in a temporary variable.
+            head = head.next  # Make the head as the next node in the list
+            temp.next = reversed  # Make the stored elements next point to the first element in the reversed variable.
+            reversed = temp  # Make reversed as the new linked list with the temp variable added in reverse.
+        return reversed
