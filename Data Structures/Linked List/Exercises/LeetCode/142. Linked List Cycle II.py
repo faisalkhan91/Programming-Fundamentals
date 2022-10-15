a4bcd81b9a2c0d7aa7e49f5e6631e78a8a2c3e1f
Faisal Khan <faisalkhan91@outlook.com>
2022-10-14 20:33:33 -0400

    142. Linked List Cycle II

--- Data Structures/Linked List/Exercises/LeetCode/142. Linked List Cycle II.py
+++ Data Structures/Linked List/Exercises/LeetCode/142. Linked List Cycle II.py
@@ -1,3 +1,39 @@
 """
 142. Linked List Cycle II
+https://leetcode.com/problems/linked-list-cycle-ii/?envType=study-plan&id=level-1
 """
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+
+
+
+
+
+
+
+# Initial Attempt, does not work on all edge cases, example: [-1,-7,7,-4,19,6,-9,-5,-2,-5], 6, since the method used
+# initially was based on assumption that all the values are unique.
+# class Solution:
+#     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         node_map = {}
+#         if not head:
+#             return
+#         if head.next is None:
+#             return
+#         while head:
+#             if head.val not in node_map and head.next is not None:
+#                 node_map[head.val] = head.next.val
+#                 head = head.next
+#             elif head.next is None:
+#                 print(node_map)
+#                 return
+#             else:
+#                 print(node_map)
+#                 return head
