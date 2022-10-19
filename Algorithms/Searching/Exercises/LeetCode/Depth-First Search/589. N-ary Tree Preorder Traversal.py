c97fefba8945caa22c7a65ef9a8cacbf3b7ef558
Faisal Khan <faisalkhan91@outlook.com>
2022-10-19 00:00:58 -0400

    589. N-ary Tree Preorder Traversal

--- Algorithms/Searching/Exercises/LeetCode/Depth-First Search/589. N-ary Tree Preorder Traversal.py
+++ Algorithms/Searching/Exercises/LeetCode/Depth-First Search/589. N-ary Tree Preorder Traversal.py
@@ -1,3 +1,27 @@
 """
 589. N-ary Tree Preorder Traversal
+https://leetcode.com/problems/n-ary-tree-preorder-traversal/?envType=study-plan&id=level-1
 """
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        result = []
+        self.preorder_traversal(root, result)  # Call the recursive function.
+        return result
+
+    def preorder_traversal(self, root, result):
+        # If the end of the tree, return.
+        if root == None:
+            return result
+        result.append(root.val)  # In preorder, we add the value to the output before we go to the child nodes.
+        for child in root.children:  # Call the child nodes, it will be from left to right.
+            self.preorder_traversal(child, result)
