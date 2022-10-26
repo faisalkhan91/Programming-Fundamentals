2c4cb564828d22b45d861a7d4055c454a59ba159
Faisal Khan <faisalkhan91@outlook.com>
2022-10-26 00:15:59 -0400

    704. Binary Search

--- Algorithms/Searching/Exercises/LeetCode/Binary Search/704. Binary Search.py
+++ Algorithms/Searching/Exercises/LeetCode/Binary Search/704. Binary Search.py
@@ -4,3 +4,25 @@ https://leetcode.com/problems/binary-search/
 """
 
 
+class Solution:
+    def search(self, nums, target):
+
+        low = 0  # Set low as index 0
+        high = len(nums) - 1  # Set high as length of array - 1
+
+        while low <= high:
+            mid = (low + high) // 2  # Calculate the mid
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:  # If the value at mid-index is less than the target value search on the right.
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
+
+
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+execute = Solution()
+print(execute.search(nums, target))
