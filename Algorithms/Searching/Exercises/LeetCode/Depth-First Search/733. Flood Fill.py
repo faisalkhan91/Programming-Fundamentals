484e2cb517ee183848829d61b4323939a96e59da
Faisal Khan <faisalkhan91@outlook.com>
2022-10-29 20:21:44 -0400

    733. Flood Fill

--- Algorithms/Searching/Exercises/LeetCode/Depth-First Search/733. Flood Fill.py
+++ Algorithms/Searching/Exercises/LeetCode/Depth-First Search/733. Flood Fill.py
@@ -5,20 +5,37 @@ https://leetcode.com/problems/flood-fill/
 
 
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        return self.modified_image(image, sr, sc, color)
+    def floodFill(self, image, sr: int, sc: int, color: int):
+        """
+        The method uses DFS to visit each pixel and make changes according to the conditions below. We increment or
+        decrement the rows and columns and check for modification based on the preset conditions.
+        """
+        start = image[sr][sc]  # Store initial value of the starting pixel so we can compare other pixels like it.
+        return self.modified_image(image, sr, sc, color, start)
 
-    def modified_image(self, image, row, column, color):
-        m = len(image)
-        n = len(image[0])
-        if row < 0 or row > m - 1 or column < 0 or column > n - 1 or image[row][column] == color or image[row][
-            column] == 0:
+    def modified_image(self, image, row, column, color, start):
+        m = len(image)  # Calculate the number of rows.
+        n = len(image[0])  # Calculate the number of columns.
+        if row < 0 or row > m - 1 or column < 0 or column > n - 1:  # If the row or columns exceeds the index range.
             return
-        print("row", row, "column", column)
-        image[row][column] = color
-        self.modified_image(image, row + 1, column, color)
-        self.modified_image(image, row - 1, column, color)
-        self.modified_image(image, row, column + 1, color)
-        self.modified_image(image, row, column - 1, color)
+        if image[row][column] != start:  # If the pixel value does not match the start pixel
+            return
+        if image[row][column] == color:  # If the pixel already has the needed color.
+            return image
+        image[row][column] = color  # Set the new color for the current pixel.
+        # Move the pixel 4-directionally using recursion as below.
+        self.modified_image(image, row + 1, column, color, start)
+        self.modified_image(image, row - 1, column, color, start)
+        self.modified_image(image, row, column + 1, color, start)
+        self.modified_image(image, row, column - 1, color, start)
 
         return image
+
+
+image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+sr = 1
+sc = 1
+color = 2
+
+execute = Solution()
+print(execute.floodFill(image, sr, sc, color))
