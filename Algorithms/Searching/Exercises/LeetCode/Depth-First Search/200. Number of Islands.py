8effef4206d0bc25084e613b256f016855b14235
Faisal Khan <faisalkhan91@outlook.com>
2022-11-05 16:21:42 -0400

    200. Number of Islands

--- Algorithms/Searching/Exercises/LeetCode/Depth-First Search/200. Number of Islands.py
+++ Algorithms/Searching/Exercises/LeetCode/Depth-First Search/200. Number of Islands.py
@@ -6,19 +6,32 @@ https://leetcode.com/problems/number-of-islands/
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        return self.islands(grid, 0, 0, 0, grid[0][0])
+        m = len(grid)
+        n = len(grid[0])
+        num_of_islands = 0
+        for row in range(m):
+            for column in range(n):
+                if grid[row][column] == "1":
+                    num_of_islands += 1
+                    self.visit_island(grid, row, column)
 
-    def islands(self, grid, row, column, count, prev):
+        return num_of_islands
+
+    def visit_island(self, grid, row, column):
         m = len(grid)
         n = len(grid[0])
         if row < 0 or row > m - 1 or column < 0 or column > n - 1:
             return
         if grid[row][column] == "0":
             return
-        if grid[row][column] == prev:
-            count += 1
+        if grid[row][column] == "2":
+            return
+        if grid[row][column] == "1":
+            grid[row][column] = "2"
         print("row", row, "column", column)
-        self.islands(grid, row + 1, column, count, prev)
-        self.islands(grid, row - 1, column, count, prev)
-        self.islands(grid, row, column + 1, count, prev)
-        self.islands(grid, row, column - 1, count, prev)
+        self.visit_island(grid, row + 1, column)
+        self.visit_island(grid, row - 1, column)
+        self.visit_island(grid, row, column + 1)
+        self.visit_island(grid, row, column - 1)
+
+        return grid
