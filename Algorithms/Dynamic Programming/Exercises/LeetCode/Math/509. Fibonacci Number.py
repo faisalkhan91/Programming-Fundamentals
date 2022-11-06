d9b9dce2325f30af4d3e7d626bfd195b31c0b42d
Faisal Khan <faisalkhan91@outlook.com>
2022-11-06 17:40:20 -0500

    509. Fibonacci Number

--- Algorithms/Dynamic Programming/Exercises/LeetCode/Math/509. Fibonacci Number.py
+++ Algorithms/Dynamic Programming/Exercises/LeetCode/Math/509. Fibonacci Number.py
@@ -1,3 +1,27 @@
 """
 509. Fibonacci Number
+https://leetcode.com/problems/fibonacci-number/
+Check the fibonacci.py file for explanation.
 """
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        cache = {}  # To store the already calculated values.
+
+        # Nested function.
+        def fib_calculator(n):
+            if n in cache:
+                return cache[n]
+            elif n < 2:
+                cache[n] = n
+                return n
+            else:
+                cache[n] = fib_calculator(n-1) + fib_calculator(n-2)
+                return cache[n]
+        return fib_calculator(n)
+
+
+n = 10
+execute = Solution()
+print(execute.fib(n))
