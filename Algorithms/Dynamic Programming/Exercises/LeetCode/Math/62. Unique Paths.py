e4b80c2c6e01dd3357130acebee3fb84b9ba45b5
Faisal Khan <faisalkhan91@outlook.com>
2022-11-14 23:45:29 -0500

    62. Unique Paths

--- Algorithms/Dynamic Programming/Exercises/LeetCode/Math/62. Unique Paths.py
+++ Algorithms/Dynamic Programming/Exercises/LeetCode/Math/62. Unique Paths.py
@@ -1,4 +1,23 @@
 """
 62. Unique Paths
-
+https://leetcode.com/problems/unique-paths/description/?envType=study-plan&id=level-1
 """
+
+
+################### Still working, trying alternative solution.
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        store_paths = [0]
+
+        def paths(m, n):
+
+            if m == 0 and n == 0:
+                store_paths[0] += 1
+            if m < 0 or n < 0:
+                return
+            paths(m - 1, n)
+            paths(m, n - 1)
+
+            return store_paths
+
+        return paths(m, n)
