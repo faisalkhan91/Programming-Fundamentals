8cac01b2bde0bc7611d861859095974405526d72
Faisal Khan <faisalkhan91@outlook.com>
2022-11-18 22:37:02 -0500

    62. Unique Paths

--- Algorithms/Dynamic Programming/Exercises/LeetCode/Math/62. Unique Paths.py
+++ Algorithms/Dynamic Programming/Exercises/LeetCode/Math/62. Unique Paths.py
@@ -3,6 +3,26 @@
 https://leetcode.com/problems/unique-paths/description/?envType=study-plan&id=level-1
 """
 
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        completed_paths = [0]
+        def routes(i, j):
+            if i == m - 1 and j == n - 1:
+                completed_paths[0] += 1
+                return
+            if i > m - 1 or j > n - 1:
+                return
+            routes(i + 1, j)
+            routes(i, j + 1)
+        routes(0, 0)
+        return completed_paths[0]
+
+
+m = 2
+n = 3
+execute = Solution()
+execute.uniquePaths(m, n)
+
 
 ################### Still working, trying alternative solution.
 class Solution:
@@ -30,4 +50,30 @@ class Solution:
 
     for i in range(m):
         for j in range(n):
-            
+
+    #############################
+
+    class Solution:
+        def uniquePaths(self, m: int, n: int) -> int:
+            completed_paths = [0]
+            check = []
+
+            def routes(i, j):
+                print(i, j)
+                print(check)
+
+                if i == m - 1 and j == n - 1:
+                    completed_paths[0] += 1
+                    return
+                if i > m - 1 or j > n - 1:
+                    return
+                if (i, j) not in check:
+                    check.append((i, j))
+                    routes(i + 1, j)
+                    routes(i, j + 1)
+                else:
+                    return
+
+            routes(0, 0)
+            print(len(check))
+            return completed_paths[0]
