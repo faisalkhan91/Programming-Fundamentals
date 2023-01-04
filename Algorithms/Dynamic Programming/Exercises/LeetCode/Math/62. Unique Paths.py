5b475ea653779ccdf32262cbd0451613e4069e15
Faisal Khan <faisalkhan91@outlook.com>
2023-01-03 19:55:03 -0500

    62. Unique Paths

--- Algorithms/Dynamic Programming/Exercises/LeetCode/Math/62. Unique Paths.py
+++ Algorithms/Dynamic Programming/Exercises/LeetCode/Math/62. Unique Paths.py
@@ -21,21 +21,23 @@ class Solution:
         routes(0, 0)  # Start with the 0 index.
         return completed_paths[0]
 
-    # Brute force alternative approach.
+    # Brute force alternative approach. In this approach a grid is initialized with m and n and the bottom right corner
+    # is set to 1. Then a nested method is used to loop through the grid recursively until it reaches the bottom right
+    # corner. It has the same pitfall as the above method as in once the grid gets large enough the algorithm fails.
     def uniquePaths_bruteforce_2(self, m: int, n: int) -> int:
         completed_paths = [0]
-        grid = [[0 for i in range(n)] for j in range(m)]
-        grid[m - 1][n - 1] = 1
+        grid = [[0 for i in range(n)] for j in range(m)]  # Initialize a 2D array of size m x n.
+        grid[m - 1][n - 1] = 1  # Set the bottom right corner to 1.
 
         def routes(i, j):
-            if i > m - 1 or j > n - 1:
+            if i > m - 1 or j > n - 1:  # If the indexes are out of range stop processing.
                 return
-            if grid[i][j] == 1:
+            if grid[i][j] == 1:  # If the bottom right corner is reached save the path.
                 completed_paths[0] += 1
                 return
             routes(i + 1, j)
             routes(i, j + 1)
-        routes(0, 0)
+        routes(0, 0)  # Start from the top left corner.
         return completed_paths[0]
 
 
