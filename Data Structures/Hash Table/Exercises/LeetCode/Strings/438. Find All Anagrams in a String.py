9a981dc0312ff48ea1159aaeb8859c1fa07f791f
Faisal Khan <faisalkhan91@outlook.com>
2023-01-30 23:54:49 -0500

    438. Find All Anagrams in a String

--- Data Structures/Hash Table/Exercises/LeetCode/Strings/438. Find All Anagrams in a String.py
+++ Data Structures/Hash Table/Exercises/LeetCode/Strings/438. Find All Anagrams in a String.py
@@ -3,5 +3,17 @@
 https://leetcode.com/problems/find-all-anagrams-in-a-string/?envType=study-plan&id=level-1
 """
 
-
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        checker = list(p)
+        anagram_index = []
+        temp = []
+        for i in range(len(s)):
+            if s[i] in checker and len(checker) != 0:
+                checker.pop(s[i])
+                temp.append(i)
+            elif len(checker) == 0:
+                checker = list(p)
+                anagram_index.append(temp[0])
+                temp = []
 
