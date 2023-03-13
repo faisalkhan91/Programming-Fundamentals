e4f62467471f974c58803b38faf1d793664c684c
Faisal Khan <faisalkhan91@outlook.com>
2023-03-12 23:40:31 -0400

    438. Find All Anagrams in a String

--- Data Structures/Hash Table/Exercises/LeetCode/Strings/438. Find All Anagrams in a String.py
+++ Data Structures/Hash Table/Exercises/LeetCode/Strings/438. Find All Anagrams in a String.py
@@ -3,33 +3,24 @@
 https://leetcode.com/problems/find-all-anagrams-in-a-string/?envType=study-plan&id=level-1
 """
 
+from collections import Counter
+
+
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        checker = list(p)
+    def findAnagrams(self, s: str, p: str):
+
+        s_substrings = {}
+        for i in range(len(s) - len(p) + 1):
+            s_substrings[i] = s[i:len(p) + i]
         anagram_index = []
-        temp = []
-        for i in range(len(s)):
-            if s[i] in checker and len(checker) != 0:
-                checker.pop(s[i])
-                temp.append(i)
-            elif len(checker) == 0:
-                checker = list(p)
-                anagram_index.append(temp[0])
-                temp = []
+        for i in s_substrings:
+            print(s_substrings[i], p)
+            if Counter(s_substrings[i]) == Counter(p):
+                anagram_index.append(i)
         return anagram_index
 
-    class Solution:
-        def findAnagrams(self, s: str, p: str) -> List[int]:
 
-            s_substrings = {}
-            for i in range(len(s) - len(p) + 1):
-                s_substrings[i] = s[i:len(p) + i]
-            print(s_substrings)
-            anagram_index = []
-            for i in s_substrings:
-                print(s_substrings[i])
-                if hash(s_substrings[i]) == hash(p):
-                    anagram_index.append(0)
-            print(anagram_index)
-
-
+s = "ababababab"
+p = "aab"
+execute = Solution()
+print(execute.findAnagrams(s, p))
