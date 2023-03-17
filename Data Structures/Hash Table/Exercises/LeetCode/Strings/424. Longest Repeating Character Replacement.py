68b4cf72f335ad0b325791187c1f97ed9839ab55
Faisal Khan <faisalkhan91@outlook.com>
2023-03-16 23:34:25 -0400

    424. Longest Repeating Character Replacement

--- Data Structures/Hash Table/Exercises/LeetCode/Strings/424. Longest Repeating Character Replacement.py
+++ Data Structures/Hash Table/Exercises/LeetCode/Strings/424. Longest Repeating Character Replacement.py
@@ -2,3 +2,11 @@
 424. Longest Repeating Character Replacement
 https://leetcode.com/problems/longest-repeating-character-replacement/?envType=study-plan&id=level-1
 """
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        start_index = 0
+        end_index = 0
+        for i range(len(s)):
+             start_index = i
+             if flag
