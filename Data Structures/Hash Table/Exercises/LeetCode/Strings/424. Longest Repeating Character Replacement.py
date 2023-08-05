6f8f75cb71f209e8fc1e1dbdb8355a9f1b90b785
Faisal Khan <faisalkhan91@outlook.com>
2023-08-05 17:01:22 -0400

    424. Longest Repeating Character Replacement

--- Data Structures/Hash Table/Exercises/LeetCode/Strings/424. Longest Repeating Character Replacement.py
+++ Data Structures/Hash Table/Exercises/LeetCode/Strings/424. Longest Repeating Character Replacement.py
@@ -35,64 +35,42 @@ class Solution:
         return max_string
 
 
+    def characterReplacement2(self, s: str, k: int) -> int:
+        """
+        Reference: https://www.youtube.com/watch?v=gqXU1UyA8pk
+        This is the implementation of the solution using sliding windows technique and a hash map. We use the left and
+        right pointers to keep track of the sliding window. We keep incrementing the right pointer and calculating the
+        frequency of the characters.
 
+        The formula used to check for a valid substring would be:
+        Length of the sliding window - Maximum frequency of the most frequent character <= k
 
-#----------------------------------------------------------------#
+        If the above condition fails we move the left pointer to the right to have a valid substring.
 
-    def characterReplacement(self, s: str, k: int) -> int:
+        :param s:
+        :param k:
+        :return: res
+        """
+        count = {}  # Create a hash map to store all the frequency of the letters.
+        res = 0  # To store the result.
 
-        substring_frequency = {}
-        count = 1
-        max = 0
-        max_dict = {}
+        left = 0  # Set the left pointer if the sliding window to 0.
 
-        for index in range(len(s) - 1):
-            # if s[index] in substring_frequency and s[index] == s[index - 1]:
-            #     substring_frequency[s[index]][s[index]] += 1
-            # else:
-            #     substring_frequency[s[index]] = {s[index] + str(index): 1}
+        for right in range(len(s)):  # Increment the right pointer of the sliding window.
+            count[s[right]] = 1 + count.get(s[right], 0)  # At each iteration update the frequency of the characters.
 
-            print(index, len(s))
-            if s[index] == s[index + 1]:
-                count += 1
-            else:
-                if s[index] not in substring_frequency:
-                    substring_frequency[s[index]] = {}
-                    substring_frequency[s[index]].update({s[index] + str(index): count})
-                else:
-                    substring_frequency[s[index]].update({s[index] + str(index): count})
-                count = 1
-                if index + 1 == len(s) - 1:
-                    substring_frequency[s[index + 1]].update({s[index + 1] + str(index): count})
-            if count > max:
-                max = count
-                max_dict.update()
+            # Increase the left pointer in the sliding window if the formula exceeds k. ((right-left)+1), there is + 1
+            # because the starting indexes of left and right is 0 but the window size is 1.
+            if ((right - left) + 1) - max(count.values()) > k:
+                count[s[left]] -= 1  # Decrease the letter frequency of the character at left pointer.
+                left += 1  # Move the left pointer to the right.
 
-        print(substring_frequency)
+            # Store the max result between previous result and current sliding window.
+            res = max(res, (right - left) + 1)
 
-        return 0
+        return res
 
-
-
-    #-------------------------------------------------------------------#
-
-    class Solution:
-        def characterReplacement(self, s: str, k: int) -> int:
-            first = 0
-            second = 1
-            count = 1
-            max = 0
-            while second > first and second < len(s):
-                if s[first] == s[second]:
-                    count += 1
-                    second += 1
-                elif s[first] != s[second] and k != 0:
-                    count += 1
-                    k -= 1
-                else:
-                    first += 1
-
-                if max < count:
-                    max = count
-                print(first, second, s[first:second], count)
-            return max
+s = "AABABBA"
+k = 1
+execute = Solution()
+print(execute.characterReplacement2(s, k))
