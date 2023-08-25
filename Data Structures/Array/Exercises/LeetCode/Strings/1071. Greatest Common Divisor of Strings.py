8fdc99c7d9bfcc4c7535b4500b614cc677939fab
Faisal Khan <faisalkhan91@outlook.com>
2023-08-25 17:03:42 -0400

    1071. Greatest Common Divisor of Strings.

--- Data Structures/Array/Exercises/LeetCode/Strings/1071. Greatest Common Divisor of Strings.py
+++ Data Structures/Array/Exercises/LeetCode/Strings/1071. Greatest Common Divisor of Strings.py
@@ -7,7 +7,30 @@ from collections import Counter
 
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        pass
+        """
+        In this solution the string is checked if it s divisible by calculating the factor.
+        :param str1:
+        :param str2:
+        :return: str[i], ""
+        """
+
+        len1, len2 = len(str1), len(str2)  # Pre-calculate the length of the strings.
+
+        # Create helper function, to find the divisor of the string.
+        def is_divisor(l):
+            # Check if the string is divisible by yhe current length but taking modulus and seeing if the mod return 0.
+            # For example if len % l == 0, then the string is divisible and it WON'T return false.
+            if len1 % l or len2 % l:
+                return False
+            f1, f2 = len1 // l, len2 // l  # Calculate the factor of each string.
+            # Multiply str1 with the factor and see if it produces str1 and str2 and return True.
+            return str1[:l] * f1 == str1 and str1[:l] * f2 == str2
+
+        for i in range(min(len1, len2), 0, -1): # We start with the reverse of the smallest string.
+            if is_divisor(i):
+                return str1[:i]
+
+        return ""  # Return default if no divisible string found.
 
 
 #############################################################################
@@ -61,7 +84,8 @@ class Solution:
 #     return str1[:GCD]
 
 # This soluition only works partially and fails for test cases such as,
-# str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX", str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+# str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX", str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX", as it doesn't account for
+# duplicate letters like above 'XX'.
 #
 # def gcdOfStrings(self, str1: str, str2: str) -> str:
 #     str1_freq = Counter(str1)
@@ -82,5 +106,24 @@ class Solution:
 #
 #     # if (max(len(str1), len(str2)) % GCD) != 0:
 #     #     GCD = GCD + (max(len(str1), len(str2)) % GCD)
+#
+#     return str1[:GCD]
+
+# Another one that fails for another test case.
+#
+# def gcdOfStrings(self, str1: str, str2: str) -> str:
+#
+#     if str1 + str2 != str2 + str1:
+#         return ""
+#
+#     str1_freq = Counter(str1)
+#     str2_freq = Counter(str2)
+#     GCD = 0
+#
+#     for i in str1_freq:
+#         if str1_freq[i] == 1 or str2_freq[i] == 1:
+#             GCD += 1
+#         else:
+#             GCD = GCD + min(str1_freq[i] // len(str1_freq), str2_freq[i] // len(str2_freq))
 #
 #     return str1[:GCD]
