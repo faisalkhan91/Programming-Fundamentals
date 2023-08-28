d550d92dae19cd7924ebbbeaf7dcdf9a73fb46e3
Faisal Khan <faisalkhan91@outlook.com>
2023-08-28 19:47:48 -0400

    238. Product of Array Except Self

--- Data Structures/Array/Exercises/LeetCode/Prefix Sum/238. Product of Array Except Self.py
+++ Data Structures/Array/Exercises/LeetCode/Prefix Sum/238. Product of Array Except Self.py
@@ -5,6 +5,8 @@ https://leetcode.com/problems/product-of-array-except-self/description/?envType=
 
 
 class Solution:
+    # This fails due to time limit exceeded. But solution is straightforward, the left and right is calculated for each
+    # number and added together.
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         left = 0
         right = len(nums) - 1
@@ -30,7 +32,35 @@ class Solution:
                 right = len(nums) - 1
         return answer
 
+    def productExceptSelfOptimized(self, nums: List[int]) -> List[int]:
+        """
+        In this solution, the product from left to right is calculated, and then product from right to left is
+        calculated, then we calculate the answer as a product of left product and right product.
+        :param nums:
+        :return: answer
+        """
+        left_prod = len(nums) * [1]
+        right_prod = len(nums) * [1]
+        answer = len(nums) * [1]
+
+        left_prod[0] = nums[0]
+
+        # Calculate left product.
+        for i in range(1, len(nums)):
+            left_prod[i] = left_prod[i - 1] * nums[i]
+
+        # Calculate right product.
+        for i in range(len(nums) - 1, 0, -1):
+            right_prod[i - 1] = right_prod[i] * nums[i]
+
+        # Calculate the product for each index in the nums array.
+        answer[0] = right_prod[0]
+        for i in range(1, len(answer)):
+            answer[i] = left_prod[i - 1] * right_prod[i]
+
+        return answer
+
 
 nums = [1,2,3,4]
 execute = Solution()
-print(execute.productExceptSelf(nums))
+print(execute.productExceptSelfOptimized(nums))
