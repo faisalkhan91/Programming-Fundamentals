43be5b96f967c9c4f8a0235bee1ac4e03c63ba20
Faisal Khan <faisalkhan91@outlook.com>
2023-08-30 20:13:50 -0400

    Amazon interview question

---
+++ Interview/Array random letter in string.py
@@ -0,0 +1,20 @@
+
+
+def similar_word():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+letter = ogd
+list_of_words = [cab, ab, go, od, odg, god, good, back]
+
+# Return a list of all the words in the list where the random letter mathces the word but letter frequency should be same
+# or 0. Output = [go, od, odg, god]
