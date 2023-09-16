dec4f803d0183b8acda36666e007e16070a728e9
Faisal Khan <faisalkhan91@outlook.com>
2023-09-16 19:47:47 -0400

    643. Maximum Average Subarray I.

--- Algorithms/Techniques/Sliding Window/LeetCode/Array/643. Maximum Average Subarray I.py
+++ Algorithms/Techniques/Sliding Window/LeetCode/Array/643. Maximum Average Subarray I.py
@@ -6,22 +6,57 @@ https://leetcode.com/problems/maximum-average-subarray-i/description/?envType=st
 
 class Solution:
     def findMaxAverage(self, nums, k: int) -> float:
+        """
+        This solution uses the sliding window technique.
+        :param nums:
+        :param k:
+        :return:
+        """
 
         left = 0
-        max_avg = 0
+        right = k
+        current_sum = sum(nums[left:right])
+        max_avg = current_sum / k
+
+        if len(nums) == k:
+            return max_avg
+
+        while right < len(nums):
+            current_sum = current_sum + nums[right] - nums[left]
+            current_avg = current_sum / k
+            max_avg = max(max_avg, current_avg)
+            left += 1
+            right += 1
+
+        return max_avg
+
+    def findMaxAverage_initial(self, nums, k: int) -> float:
+        """
+        This solution is slow and ends in time limit exceeded.
+        :param nums:
+        :param k:
+        :return:
+        """
+        left = 0
+        max_avg = float("-inf")
         current_sum = 0
+        current_avg = 0
+
+        if len(nums) == k:
+            return sum(nums) / k
 
         for i in range(len(nums)):
             if len(nums[:i]) < k:
                 current_sum += nums[i]
-                current_avg = current_sum / k
+                if len(nums[:i]) == k - 1:
+                    current_avg = current_sum / k
             else:
                 current_sum = current_sum + nums[i] - nums[left]
                 current_avg = current_sum / k
                 left += 1
-
-            if max_avg < current_sum:
-                max_avg = current_sum
+            print(current_sum, current_avg, max_avg, k)
+            if max_avg < current_avg:
+                max_avg = current_avg
 
         return max_avg
 
