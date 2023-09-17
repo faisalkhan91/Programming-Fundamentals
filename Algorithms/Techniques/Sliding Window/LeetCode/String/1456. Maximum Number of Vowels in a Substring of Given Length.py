aad3a5f22f93084dd45048b9b8982dabd890cbc1
Faisal Khan <faisalkhan91@outlook.com>
2023-09-16 20:56:49 -0400

    1456. Maximum Number of Vowels in a Substring of Given Length.

--- Algorithms/Techniques/Sliding Window/LeetCode/String/1456. Maximum Number of Vowels in a Substring of Given Length.py
+++ Algorithms/Techniques/Sliding Window/LeetCode/String/1456. Maximum Number of Vowels in a Substring of Given Length.py
@@ -6,22 +6,33 @@ https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-l
 
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
+        """
+        In this solution the sliding window technique is used to traverse the string. The initial count of the vowels is
+        calculated by the given window size and then a while loop is used to move the window to the right, each letter
+        entering the string is checked for the vowel parameters, if it is a vowel the current_vowel count is incremented
+        by 1, similarly if the letter leaving the window is a vowel the current count is decremented. Each iteration is
+        compared with the previous max count and stored accordingly.
+        :param s:
+        :param k:
+        :return: max_vowels
+        """
         left = 0
         right = k
         vowels = ['a', 'e', 'i', 'o', 'u']
-        current_vowels = max_vowels = 0
+        max_vowels = 0
 
+        # Calculate the count of the initial window.
         for i in range(k):
             if s[i] in vowels:
                 max_vowels += 1
 
-        current_vowels = max_vowels
+        current_vowels = max_vowels  # Set the current count as the previous max count.
         while right < len(s):
             if s[right] in vowels:
                 current_vowels += 1
             if s[left] in vowels:
                 current_vowels -= 1
-            max_vowels = max(max_vowels, current_vowels)
+            max_vowels = max(max_vowels, current_vowels)  # Compare current count in each iteration.
             right += 1
             left += 1
 
