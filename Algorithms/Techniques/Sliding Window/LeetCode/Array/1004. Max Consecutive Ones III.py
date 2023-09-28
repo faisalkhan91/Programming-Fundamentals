333df437e97df6e98998d381624b4bf71569cb77
Faisal Khan <faisalkhan91@outlook.com>
2023-09-28 12:19:45 -0400

    1004. Max Consecutive Ones III

--- Algorithms/Techniques/Sliding Window/LeetCode/Array/1004. Max Consecutive Ones III.py
+++ Algorithms/Techniques/Sliding Window/LeetCode/Array/1004. Max Consecutive Ones III.py
@@ -102,3 +102,27 @@ class Solution:
         return max_ones
 
 
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+
+        left = 0
+        right = 0
+        max_ones = 0
+
+        while right < len(nums) - 1:
+            if k > 0:
+                if nums[right] == 1:
+                    right += 1
+                else:
+                    k -= 1
+                    right += 1
+                    left += 1
+            else:
+                if nums[left] == 0:
+                    k += 1
+                left += 1
+                right += 1
+            print(left, right, max_ones, nums[left:right])
+            max_ones = max(max_ones, right - left)
+
+        return max_ones
