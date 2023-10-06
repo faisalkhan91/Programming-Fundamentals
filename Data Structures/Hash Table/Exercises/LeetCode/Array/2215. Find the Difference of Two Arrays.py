ba71c0be417ae5521eedbdda146a267a426c241a
Faisal Khan <faisalkhan91@outlook.com>
2023-10-06 17:14:40 -0400

    2215. Find the Difference of Two Arrays

--- Data Structures/Hash Table/Exercises/LeetCode/Array/2215. Find the Difference of Two Arrays.py
+++ Data Structures/Hash Table/Exercises/LeetCode/Array/2215. Find the Difference of Two Arrays.py
@@ -7,20 +7,24 @@ https://leetcode.com/problems/find-the-difference-of-two-arrays/description/?env
 class Solution:
     def findDifference(self, nums1, nums2):
         """
-        In this solution, Hashset is used to store the values of the numbers.
+        In this solution, Hashset is used to store the values of the numbers in the arrays. The Hashsets are compared
+        with each other and the unique values are stored into their respective temporary arrays. This resulting
+        arrays contain the difference between the arrays and are returned as the output.
         :param nums1:
         :param nums2:
-        :return:
+        :return: tmp1, tmp2
         """
-        nums1 = set(nums1)
-        nums2 = set(nums2)
+        nums1 = set(nums1)  # Convert the num1 array to Hashset.
+        nums2 = set(nums2)  # Convert to Hashset.
         tmp1 = []
         tmp2 = []
 
+        # Find the difference in num1 set.
         for i in nums1:
             if i not in nums2:
                 tmp1.append(i)
 
+        # Find the difference in the num2 set.
         for i in nums2:
             if i not in nums1:
                 tmp2.append(i)
