54950fa860e96531f257320e4afd23afdb50ae7f
Faisal Khan <faisalkhan91@outlook.com>
2023-10-09 20:41:32 -0400

    1657. Determine if Two Strings Are Close.

--- Data Structures/Hash Table/Exercises/LeetCode/Strings/1657. Determine if Two Strings Are Close.py
+++ Data Structures/Hash Table/Exercises/LeetCode/Strings/1657. Determine if Two Strings Are Close.py
@@ -3,13 +3,20 @@
 https://leetcode.com/problems/determine-if-two-strings-are-close/description/?envType=study-plan-v2&envId=leetcode-75
 """
 
+from collections import Counter
+
 
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        word1_set = set(word1)
-        word2_set = set(word2)
 
-        if word1_set == word2_set and len(word1) == len(word2):
+        word1_freq = Counter(word1)
+        word2_freq = Counter(word2)
+        word1_set = list(word1_freq.values())
+        word2_set = list(word2_freq.values())
+        word1_set.sort()
+        word2_set.sort()
+
+        if word1_set == word2_set and set(word1) == set(word2):
             return True
         return False
 
