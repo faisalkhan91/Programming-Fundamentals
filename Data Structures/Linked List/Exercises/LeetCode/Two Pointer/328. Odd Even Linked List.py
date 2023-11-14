0e9543edaef379ca5899425d159e576968ee6f13
Faisal Khan <faisalkhan91@outlook.com>
2023-11-14 18:48:30 -0500

    328. Odd Even Linked List

--- Data Structures/Linked List/Exercises/LeetCode/Two Pointer/328. Odd Even Linked List.py
+++ Data Structures/Linked List/Exercises/LeetCode/Two Pointer/328. Odd Even Linked List.py
@@ -3,3 +3,25 @@
 https://leetcode.com/problems/odd-even-linked-list/description/?envType=study-plan-v2&envId=leetcode-75
 """
 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not head: return head
+        odd = head
+        even = placeholder = head.next
+
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+
+        odd.next = placeholder
+
+        return head
