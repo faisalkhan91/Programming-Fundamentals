81eaf6cc2c3d41df88882246f4097cff901b13f6
Faisal Khan <faisalkhan91@outlook.com>
2023-11-30 20:13:53 -0500

    872. Leaf-Similar Trees

--- Algorithms/Searching/Exercises/LeetCode/Depth-First Search/872. Leaf-Similar Trees.py
+++ Algorithms/Searching/Exercises/LeetCode/Depth-First Search/872. Leaf-Similar Trees.py
@@ -12,19 +12,63 @@ https://leetcode.com/problems/leaf-similar-trees/description/?envType=study-plan
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        r1 = []
-        r2 = []
-        r1 = self.DFS(root1, r1)
-        r2 = self.DFS(root2, r2)
-        if r1 == r2:
-            return True
-        return False
+        """
+        In this solution, the Depth-First Search (DFS) technique is used to get the leaf nodes and then the leaf nodes
+        of both the trees are compared. It doesn't matter what DFS traversal is used to find the leaf nodes, but in the
+        below solution preorder traversal is used, and in the following solution in-order traversal is utilized. The
+        traversal is separated into a new method for easy understanding. Two stacks are declared that will hold the
+        values of the leaf nodes of the trees. The DFS method recursively traverses the nodes and appends the leaf
+        values to the stack. The stacks are compared and if are found to be same a boolean True is returned else False
+        is returned.
 
-    def DFS(self, root, res):
-        if not root.left and not root.right:
-            res.append(root.val)
-        if root.left:
-            self.DFS(root.left, res)
-        if root.right:
-            self.DFS(root.right, res)
-        return res
+        Time Complexity: O(n). Space Complexity: O(n).
+
+        :param root1: Binary Tree
+        :param root2: Binary Tree
+        :return: Boolean: True or False
+        """
+        leaves1 = []
+        leaves2 = []
+        self.DFS(root1, leaves1)
+        self.DFS(root2, leaves2)
+        return True if leaves1 == leaves2 else False
+
+    def DFS(self, node, leaves):
+        """
+        This is the preorder method used by the solution above.
+
+        Time Complexity: O(n). Space Complexity: O(n).
+
+        :param node: Binary Tree
+        :param leaves: All the leaves
+        :return: leaves
+        """
+        if not node.left and not node.right:
+            leaves.append(node.val)
+        if node.left:
+            self.DFS(node.left, leaves)
+        if node.right:
+            self.DFS(node.right, leaves)
+        return leaves
+
+    ############################################################################################
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        """
+        Simailar to above, but just uses inorder traversal.
+        :param root1: Binary Tree
+        :param root2: Binary Tree
+        :return: Boolean
+        """
+        l1 = []
+        l2 = []
+        self.inorder(root1, l1)
+        self.inorder(root2, l2)
+        return l1 == l2
+
+    def inorder(self, node, leaves):
+        if node:
+            self.inorder(node.left, leaves)
+            if not node.left and not node.right:
+                leaves.append(node.val)
+            self.inorder(node.right, leaves)
+        return leaves
