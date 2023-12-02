ef2d595b020ff5ff741a5ccd4244d4395ae8089e
Faisal Khan <faisalkhan91@outlook.com>
2023-12-02 12:27:20 -0500

    1448. Count Good Nodes in Binary Tree

--- Algorithms/Searching/Exercises/LeetCode/Depth-First Search/1448. Count Good Nodes in Binary Tree.py
+++ Algorithms/Searching/Exercises/LeetCode/Depth-First Search/1448. Count Good Nodes in Binary Tree.py
@@ -3,3 +3,28 @@
 https://leetcode.com/problems/count-good-nodes-in-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75
 """
 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        good_nodes = 0
+        self.DFS(root, good_nodes)
+
+        return good_nodes
+
+    def DFS(self, node, valid):
+        if not node.left or not node.right:
+            return valid
+        if node.val <= node.left.val and node.val <= node.right.val:
+            valid += 1
+        if node.left:
+            self.DFS(node.left, valid)
+        if node.right:
+            self.DFS(node.right, valid)
+        return valid
