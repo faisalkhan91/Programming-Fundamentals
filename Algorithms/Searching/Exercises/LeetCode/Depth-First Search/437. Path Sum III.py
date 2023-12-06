85d9ae5a217980b258931d167d5b837341e9c68d
Faisal Khan <faisalkhan91@outlook.com>
2023-12-05 19:15:57 -0500

    437. Path Sum III

--- Algorithms/Searching/Exercises/LeetCode/Depth-First Search/437. Path Sum III.py
+++ Algorithms/Searching/Exercises/LeetCode/Depth-First Search/437. Path Sum III.py
@@ -2,3 +2,34 @@
 437. Path Sum III
 https://leetcode.com/problems/path-sum-iii/description/?envType=study-plan-v2&envId=leetcode-75
 """
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.path = 0
+        self.path_sum = 0
+
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        return self.DFS(root, targetSum)
+
+    def DFS(self, node, target):
+        if not node:
+            return
+
+        if self.path_sum == target:
+            self.path += 1
+        if self.path_sum >= target:
+            self.path_sum = node.val
+        else:
+            self.path_sum += node.val
+
+        self.DFS(node.left, target)
+        self.DFS(node.right, target)
+
+        return self.path
