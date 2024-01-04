023c5ab07710d55e9bca213d95fdef3e64a72f56
Faisal Khan <faisalkhan91@outlook.com>
2024-01-03 19:07:50 -0500

    1161. Maximum Level Sum of a Binary Tree

--- Algorithms/Searching/Exercises/LeetCode/Breadth-First Search/1161. Maximum Level Sum of a Binary Tree.py
+++ Algorithms/Searching/Exercises/LeetCode/Breadth-First Search/1161. Maximum Level Sum of a Binary Tree.py
@@ -14,18 +14,47 @@ class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
         queue = collections.deque([root])
         level = 0
-        level_sums = {root.val: level}
+        max_sum = float('-inf')
+        min_level = 0
 
         while queue:
             current_sum = 0
-            level += 1
             for i in range(len(queue)):
                 current_node = queue.popleft()
-                if current_node:
-                    current_sum += current_node.val
+                current_sum += current_node.val
+                if current_node.left:
                     queue.append(current_node.left)
+                if current_node.right:
                     queue.append(current_node.right)
-            if current_sum != 0:
-                level_sums[current_sum] = level
+            level += 1
+            if max_sum < current_sum:
+                max_sum = current_sum
+                min_level = level
+        return min_level
 
-        return level_sums[max(level_sums)]
+###############################################################################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+#     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+#         queue = collections.deque([root])
+#         level = 0
+#         level_sums = {root.val: level}
+#
+#         while queue:
+#             current_sum = 0
+#             level += 1
+#             for i in range(len(queue)):
+#                 current_node = queue.popleft()
+#                 if current_node:
+#                     current_sum += current_node.val
+#                     queue.append(current_node.left)
+#                     queue.append(current_node.right)
+#             if current_sum != 0:
+#                 level_sums[current_sum] = level
+#
+#         return level_sums[max(level_sums)]
