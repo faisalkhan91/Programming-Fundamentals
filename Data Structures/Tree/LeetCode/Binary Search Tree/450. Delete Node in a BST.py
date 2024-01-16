9698cb67f3943a4e15b6ef0cbef4dbc7e16c7278
Faisal Khan <faisalkhan91@outlook.com>
2024-01-16 08:02:10 -0500

    450. Delete Node in a BST.

--- Data Structures/Tree/LeetCode/Binary Search Tree/450. Delete Node in a BST.py
+++ Data Structures/Tree/LeetCode/Binary Search Tree/450. Delete Node in a BST.py
@@ -34,3 +34,35 @@ class Solution:
                         root = None
                 return root
         return root
+
+
+
+#######################################################################################
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return
+        if root.val < key:
+            self.deleteNode(root.right, key)
+        elif root.val > key:
+            self.deleteNode(root.right, key)
+        else:
+            if root.val == key:
+                if not root.right and root.left:
+                    return root.left
+                elif not root.left and root.right:
+                    return root.right
+                elif not root.right and root.left:
+                    return root.right
+                else:
+                    root.val = root.right.val
+                    root.right = None
+                    return root
+        return root
